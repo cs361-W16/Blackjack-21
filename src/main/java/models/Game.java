ecf7d7c3a5864ac83984cc516777273c95c5848a
Connor Sedwick <connorsedwick@10-249-37-149.wireless.oregonstate.edu>
2016-03-09 13:37:02 -0800

    Added Player

---
+++ src/main/java/models/Game.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.ArrayList;
+
+/**
+<<<<<<< HEAD
+ * Created by connorsedwick on 3/9/16.
+ */
+public class Game {
+    public java.util.List<User> Players = new ArrayList<>();
+    
+
+public class Game {
+
+    public java.util.List<Card> deck = new ArrayList<>();
+    //public java.util.List<java.util.List<Card>> dealer = new ArrayList<>();
+    //public java.util.List<java.util.List<Card>> user = new ArrayList<>();
+
+    public Game(){
+        User player = new Player;
+        User dealer = new Dealer;
+    }
+
+    public void getHit(){
+
+    }
+
+    public void getStay(){
+
+    }
+
+    public void getSplit(){
+
+    }
+
+    public void DetermineWinner(){
+
+    }
+
+}
